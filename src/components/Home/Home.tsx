26d199d746e23006830a605ec8d42746381cd8ea
sauravmondal24 <sauravmondal321@gmail.com>
2022-12-29 18:55:12 +0600

    project setup

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,25 @@
+import { Box, Flex, Heading, Image, Link, chakra } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Container } from '@chakra-ui/react';
+import Users from './Users';
+
+const Home = () => {
+	const [users, setUser] = useState([]);
+
+	useEffect(() => {
+		fetch('userData.json')
+			.then((res) => res.json())
+			.then((data) => setUser(data));
+	}, []);
+
+	return (
+		<Container maxW="7xl">
+			<Heading as="h1" size="xl" noOfLines={1} textAlign="center">
+				Interview Details
+			</Heading>
+			<Users users={users}></Users>
+		</Container>
+	);
+};
+
+export default Home;
---
+++ src/components/Home/Users.tsx
@@ -0,0 +1,131 @@
+import {
+	Box,
+	Flex,
+	Image,
+	Link,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	WrapItem,
+	chakra,
+	useDisclosure
+} from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+type UsersProps = {
+	users: {
+		id: string;
+		name: string;
+		image: string;
+		email: string;
+		phone: string;
+		interviewDate: string;
+		durationOfInterview: string;
+		marks: number;
+		profession: string;
+	}[];
+};
+
+const Users = ({ users }: UsersProps) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	console.log(users);
+
+	return (
+		<Box display="flex" alignItems="center" justifyContent="space-between">
+			{users.map((user) => (
+				<>
+					<Box>
+						<Flex
+							bg="#edf3f8"
+							_dark={{
+								bg: '#3e3e3e'
+							}}
+							p={50}
+							w="full"
+							alignItems="center"
+							justifyContent="center"
+						>
+							<Box
+								w="xs"
+								bg="white"
+								_dark={{
+									bg: 'gray.800'
+								}}
+								shadow="lg"
+								rounded="lg"
+								overflow="hidden"
+								mx="auto"
+							>
+								<Image
+									w="full"
+									h={56}
+									fit="cover"
+									src={user.image}
+									alt="avatar"
+								/>
+
+								<Box py={5} textAlign="center">
+									<Link
+										display="block"
+										fontSize="2xl"
+										color="gray.800"
+										_dark={{
+											color: 'white'
+										}}
+										fontWeight="bold"
+									>
+										{user.name}
+									</Link>
+									<chakra.span
+										fontSize="sm"
+										color="gray.700"
+										_dark={{
+											color: 'gray.200'
+										}}
+									>
+										{user.profession}
+									</chakra.span>
+									<WrapItem alignItems="center" justifyContent="center" my={3}>
+										<Button onClick={onOpen} size="sm" colorScheme="blue">
+											Interview Details
+										</Button>
+									</WrapItem>
+								</Box>
+							</Box>
+						</Flex>
+					</Box>
+					<Box>
+						<Modal isOpen={isOpen} onClose={onClose}>
+							<ModalOverlay />
+							<ModalContent>
+								<ModalHeader>{user.name}</ModalHeader>
+								<ModalCloseButton />
+								<ModalBody>
+									<Image
+										w="full"
+										h={56}
+										fit="cover"
+										src={user.image}
+										alt="avatar"
+									/>
+								</ModalBody>
+
+								<ModalFooter>
+									<Button colorScheme="blue" mr={3} onClick={onClose}>
+										Close
+									</Button>
+								</ModalFooter>
+							</ModalContent>
+						</Modal>
+					</Box>
+				</>
+			))}
+		</Box>
+	);
+};
+
+export default Users;
